a7c6a8ce4c7c7538752b27d7a9ae66a2d57d61ac
rezaoktaviansandyxx <indraokisandy1@gmail.com>
2025-10-17 12:53:15 +0700

    Create App

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        child: Row(
+          children: [
+            Column(children: [
+              Text("Country"),
+              Text("City"),
+            ]),
+            Container(
+              width: 45,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
