06e949ab9bda4d4b8b9611bc7ff468973f346dc7
rezaoktaviansandyxx <indraokisandy1@gmail.com>
2025-10-30 12:25:38 +0700

    Update UI

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -10,24 +13,41 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: Row(
-          children: [
-            Column(children: [
-              Text("Country"),
-              Text("City"),
-            ]),
-            Container(
-              width: 45,
-              height: 45,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.blue,
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Bangladesh", color: AppColors.mainColor),
+                      Row(
+                        children: [
+                          SmallText(text: "Narsingdi", color: Colors.black54),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SmallText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  double height;
+  SmallText({
+    super.key,
+    this.color = const Color(0xFFccc7c5),
+    required this.text,
+    this.size = 12,
+    this.height = 1.2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        color: color,
+        fontSize: size,
+        height: height,
+      ),
+    );
+  }
+}
